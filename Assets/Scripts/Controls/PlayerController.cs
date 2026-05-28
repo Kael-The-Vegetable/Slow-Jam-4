4dd6f45cff59209834176fa91f7553b784fc6a3f
Nathan Quist <quistn@mymacewan.ca>
2026-05-27 21:55:20 -0600

    Player Movement improved, new basic Level Elements

--- Assets/Scripts/Controls/PlayerController.cs
+++ Assets/Scripts/Controls/PlayerController.cs
@@ -1,36 +1,36 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
 
 public class PlayerCon : MonoBehaviour
 {
+    // Variables
     private Vector2 m_moveMag;
     private Vector2 m_lookMag;
     private Animator m_animator;
     Rigidbody2D m_rigidbody;
-    BoxCollider2D m_groundCheck;
-    
 
+    // Vector that pulls player down when they are supposed to be grounded
+    private Vector2 m_pull;
+    
+    // Gameplay Variables
     public float WalkSpeed = 5;
     public float JumpForce = 5;
+    
+    // Properties of the object used to check for groundedness
+    public Transform GroundCheckTransform;
+    public float GroundCheckRadius;
+    private RaycastHit2D m_groundedRay;
+    public LayerMask LevelLayer;
 
+    // Flags
     private bool m_grounded;
+    private bool m_jumping;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         m_rigidbody = GetComponent<Rigidbody2D>();
-        m_groundCheck = GetComponent<BoxCollider2D>();
-    }
-
-    void OnTriggerStay2D(Collider2D m_groundCheck)
-    {
-        m_grounded = true;
-    }
-
-    void OnTriggerExit2D(Collider2D m_groundCheck)
-    {
-        m_grounded = false;
-        Debug.Log("Airborn");
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -41,9 +41,9 @@ public class PlayerCon : MonoBehaviour
 
     public void OnJump(InputAction.CallbackContext context)
     {   
-        Debug.Log($"OnMove: {context.ReadValue<float>()}");
-        if (m_grounded)
+        if(context.started && m_grounded)
         {
+            m_jumping = true;
             m_rigidbody.linearVelocityY = JumpForce;
         }
     }
@@ -51,6 +51,32 @@ public class PlayerCon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Clears pull vector
+        m_pull = Vector2.zero;
+        // Checks if player is grounded
+        m_grounded = Physics2D.OverlapCircle(GroundCheckTransform.position, GroundCheckRadius, LevelLayer);
+
+        // Checks if player is falling
+        if((m_rigidbody.linearVelocityY <= 0))
+        {
+            m_jumping = false;
+        }
+        
+        // Snaps player to ground when falling near the ground
+        if(!(m_jumping) && m_grounded)
+        {
+            m_groundedRay = Physics2D.Raycast(GroundCheckTransform.position, Vector2.down, GroundCheckRadius, LevelLayer);
+            m_pull.y -= m_groundedRay.distance;
+            transform.Translate(m_pull);
+        }
+
+        // Applies movement to player
         m_rigidbody.linearVelocityX = m_moveMag.x * WalkSpeed;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(GroundCheckTransform.position, GroundCheckRadius);
     }
 }
